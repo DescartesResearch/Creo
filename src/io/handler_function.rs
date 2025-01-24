8a8b616683c7edfe7c9baa308a9386644c85fa04
Yannik Lubas <yannik.lubas@uni-wuerzburg.de>
2025-01-24 16:01:52 +0100

    chore: code format

--- src/io/handler_function.rs
+++ src/io/handler_function.rs
@@ -6,9 +6,9 @@ pub fn create_handler_function_registry(
     app: &creo_lib::graph::ApplicationGraph,
 ) -> Result<handler::FunctionRegistry> {
     creo_lib::handler::FunctionRegistry::new(app.iter_endpoints()).map_err(|err| {
-            Error::new(format!(
-                "failed to parse handler function!\n\tReason: {}",
-                err
-            ))
-        })
+        Error::new(format!(
+            "failed to parse handler function!\n\tReason: {}",
+            err
+        ))
+    })
 }
